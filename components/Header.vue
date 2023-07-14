2d3c86a2b61fc3bb11ee96e3b94549661daa12c1
Shofwan Hanif <shofwanhanif22@gmail.com>
2023-07-14 09:48:15 +0700

    Add: Init Project

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Dog Shop Catalogue</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+}
+</script>
+
+<style></style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header-component />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/Header.vue'
+export default {
+  components: { HeaderComponent },
+}
+</script>
+
+<style></style>
